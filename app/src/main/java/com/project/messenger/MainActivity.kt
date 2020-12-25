fe1342baebf388557c4e2cc4143a5048c341ab06
Veronika <sashamasyana@yandex.ru>
2020-12-25 23:39:20 +0300

    added registration

--- app/src/main/java/com/project/messenger/MainActivity.kt
+++ app/src/main/java/com/project/messenger/MainActivity.kt
@@ -7,7 +7,10 @@ import android.widget.EditText
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
@@ -20,25 +23,47 @@ class MainActivity : AppCompatActivity() {
 
 
         fun debug() {
-            Log.d("Register", "E-mail: $email.")
-            Log.d("Register", "Password: $password.")
-        }
-
-        val registrationButton = findViewById<Button>(R.id.button_register)!!
-        registrationButton.setOnClickListener {
-            debug()
+            Log.d(getString(R.string.TagRegister), "E-mail: $email.")
+            Log.d(getString(R.string.TagRegister), "Password: $password.")
         }
 
         val logIn = findViewById<TextView>(R.id.log_in_from_register)
         logIn.setOnClickListener {
-            Log.d("Register", "Trying to log in")
+            Log.d(getString(R.string.TagRegister), "Trying to log in")
 
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
 
-        FirebaseAuth.getInstance()
+        val registrationButton = findViewById<Button>(R.id.button_register)!!
+        registrationButton.setOnClickListener {
+            debug()
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                email.toString(),
+                password.toString()
+            ).addOnCompleteListener(this) {task ->
+                if (task.isSuccessful) {
+                    Log.d(
+                        getString(R.string.TagRegister), "New user had been registered.")
+                    return@addOnCompleteListener
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(getString(R.string.TagRegister), "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        this, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+
+
+
+        }
 
     }
+
+
 
 }
