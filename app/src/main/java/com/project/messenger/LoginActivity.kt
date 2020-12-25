1901db2cf29d735fe27bf181696a8a601710ef16
Veronika <sashamasyana@yandex.ru>
2020-12-26 02:59:50 +0300

    added successfully working registration and log in

--- app/src/main/java/com/project/messenger/LoginActivity.kt
+++ app/src/main/java/com/project/messenger/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.project.messenger
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
@@ -14,45 +15,67 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    /*lateinit var emailLogin: Editable
+    lateinit var passwordLogin: Editable*/
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val emailLogin = findViewById<EditText>(R.id.email_login).text
-        val passwordLogin = findViewById<EditText>(R.id.password_login).text
-
-
-        // Initialize Firebase Auth
         auth = Firebase.auth
-
-        fun debug() {
-            Log.d("LogIn", "E-mail: $emailLogin.")
-            Log.d("LogIn", "Password: $passwordLogin.")
-        }
 
         val loginButton = findViewById<Button>(R.id.login_button_login)!!
         loginButton.setOnClickListener {
-            debug()
+            val emailLogin = findViewById<EditText>(R.id.email_login).text
+            val passwordLogin = findViewById<EditText>(R.id.password_login).text
+
+            Log.d(getString(R.string.loginTag), "E-mail: $emailLogin.")
+            Log.d(getString(R.string.loginTag), "Password: $passwordLogin.")
+
+            auth.signInWithEmailAndPassword(
+                emailLogin.toString(),
+                passwordLogin.toString()
+            ).addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+                    Log.d(getString(R.string.loginTag), "Successfully logged in")
+                    val user = auth.currentUser
+                    //updateUI(user)
+                } else {
+                    Log.w(getString(R.string.loginTag), "Cannot perform log in", task.exception)
+                    Toast.makeText(
+                        this,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    //updateUI(null)
+                }
+
+            }
         }
 
         val register = findViewById<TextView>(R.id.login_to_register)
         register.setOnClickListener {
-            Log.d("Log in", "Trying to register")
+            Log.d(getString(R.string.loginTag), "Trying to register")
 
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
-    private lateinit var auth: FirebaseAuth
 
-    public override fun onStart() {
+    /*public override fun onStart() {
         super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
         updateUI(currentUser)
     }
 
-    private fun updateUI(currentUser: FirebaseUser?) {
-        Toast.makeText(applicationContext, "User authentication completed successfully!", Toast.LENGTH_SHORT).show()
-    }
+    private fun updateUI(user: FirebaseUser?) {
+        TODO("Not yet implemented")
+    }*/
+
+    /*private fun debug() {
+        Log.d(getString(R.string.loginTag), "E-mail: $emailLogin.")
+        Log.d(getString(R.string.loginTag), "Password: $passwordLogin.")
+    }*/
+
 }
--- app/src/main/java/com/project/messenger/MainActivity.kt
+++ app/src/main/java/com/project/messenger/MainActivity.kt
@@ -3,6 +3,7 @@ package com.project.messenger
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.widget.EditText
 import android.util.Log
 import android.widget.Button
@@ -14,18 +15,16 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    /*lateinit var name: Editable
+    lateinit var email: Editable
+    lateinit var password: Editable*/
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val email = findViewById<EditText>(R.id.email_registration).text
-        val password = findViewById<EditText>(R.id.password_registration).text
-
-
-        fun debug() {
-            Log.d(getString(R.string.TagRegister), "E-mail: $email.")
-            Log.d(getString(R.string.TagRegister), "Password: $password.")
-        }
+        auth = Firebase.auth
 
         val logIn = findViewById<TextView>(R.id.log_in_from_register)
         logIn.setOnClickListener {
@@ -37,33 +36,62 @@ class MainActivity : AppCompatActivity() {
 
         val registrationButton = findViewById<Button>(R.id.button_register)!!
         registrationButton.setOnClickListener {
-            debug()
-
-            FirebaseAuth.getInstance().createUserWithEmailAndPassword(
-                email.toString(),
-                password.toString()
-            ).addOnCompleteListener(this) {task ->
-                if (task.isSuccessful) {
-                    Log.d(
-                        getString(R.string.TagRegister), "New user had been registered.")
-                    return@addOnCompleteListener
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(getString(R.string.TagRegister), "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(
-                        this, "Authentication failed.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-
-
-
-
+            registration()
         }
 
     }
 
+    private fun debug() {
 
+    }
+
+    private fun registration() {
+        //debug()
+
+        val name = findViewById<EditText>(R.id.username_registration).text
+        val email = findViewById<EditText>(R.id.email_registration).text
+        val password = findViewById<EditText>(R.id.password_registration).text
+
+        if (name.isEmpty() or email.isEmpty() or password.isEmpty()) {
+            Toast.makeText(
+                applicationContext,
+                "Fields cannot be empty",
+                Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
+
+        Log.d(getString(R.string.TagRegister), "E-mail: $email.")
+        Log.d(getString(R.string.TagRegister), "Password: $password.")
+
+        auth.createUserWithEmailAndPassword(
+            email.toString(),
+            password.toString()
+        ).addOnCompleteListener(this) {task ->
+            if (task.isSuccessful) {
+                Log.d(getString(R.string.TagRegister), "New user had been registered.")
+                val user = auth.currentUser
+                //updateUI(user)
+            } else {
+                Log.w(getString(R.string.TagRegister), "Failed to create a new user", task.exception)
+                Toast.makeText(
+                    this,
+                    "Authentication failed.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                //updateUI(null)
+            }
+        }
+    }
+
+    /*private fun updateUI(user: FirebaseUser?) {
+        TODO("Not yet implemented")
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }*/
 
 }
